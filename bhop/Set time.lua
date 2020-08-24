78d963fb6c928c6cb0e547293da5e19dc6444cef
CoolestDoggo <40402235+CoolestDoggo@users.noreply.github.com>
2020-08-24 03:20:07 -0400

    Create Set time.lua

---
+++ bhop/Set time.lua
@@ -0,0 +1,17 @@
+local call
+
+local function find(name)
+    return game.Workspace:FindFirstChild(name, true)
+end
+
+for _, t in pairs(getreg()) do
+    if type(t) == 'table' and t['Call'] then
+        call = t['Call']
+        break
+    end
+end
+
+call('LeaveZone', find('MapFinish'), tick())
+call('EnterZone', find('MapStart'), tick())
+call('LeaveZone', find('MapStart'), tick())
+call('EnterZone', find('MapFinish'), tick()+69.696)
