0ae07f05ec3b7f3aef9d5998eff45a5913fe68c6
CoolestDoggo <40402235+CoolestDoggo@users.noreply.github.com>
2020-08-24 00:59:48 -0400

    Changed formatting and other stuff

--- bhop/Trigger breaker.lua
+++ bhop/Trigger breaker.lua
@@ -1,5 +1,5 @@
 for _,t in pairs(getgc(true)) do 
-    if type(t) == 'table' and t['data'] and t['data']['Trigger'] then
-        t.data.Trigger = ''
-    end
+	if type(t) == 'table' and t['data'] and t['data']['Trigger'] then
+		t.data.Trigger = ''
+	end
 end
