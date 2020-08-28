7f6191a7735cae8457e2fc5658511c82b184f303
CoolestDoggo <40402235+CoolestDoggo@users.noreply.github.com>
2020-08-28 09:46:30 -0400

    Update Nominate all maps.lua

--- bhop/Nominate all maps.lua
+++ bhop/Nominate all maps.lua
@@ -9,7 +9,7 @@ end
 
 for _, t in pairs(getgc(true)) do
 	if type(t) == 'table' and rawget(t, 'AssetId') then
-		wait()
+		wait() -- if no wait then some of the maps wont show in chat
 		call('NominateMap', t['AssetId'])
 	end
 end
