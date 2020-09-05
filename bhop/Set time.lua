4585437f17dda149b672dd29f2c310c153a31949
CoolestDoggo <40402235+CoolestDoggo@users.noreply.github.com>
2020-09-04 22:04:23 -0400

    Update Set time.lua

--- bhop/Set time.lua
+++ bhop/Set time.lua
@@ -1,14 +1,14 @@
 local call
 
 local function find(name)
-    return game.Workspace:FindFirstChild(name, true)
+	return game.Workspace:FindFirstChild(name, true)
 end
 
 for _, t in pairs(getreg()) do
-    if type(t) == 'table' and t['Call'] then
-        call = t['Call']
-        break
-    end
+	if type(t) == 'table' and t['Call'] then
+		call = t['Call']
+		break
+	end
 end
 
 call('LeaveZone', find('MapFinish'), tick())
